e789617bf3e18223b037030408bfcdf5436607a6
AlexKTP <alexandre.malachin@gmail.com>
2023-03-01 20:32:13 +0100

    Migration to Mysql with Exposed ORM

---
+++ src/main/kotlin/ktp/fr/plugins/DatabaseInitialization.kt
@@ -0,0 +1,20 @@
+package ktp.fr.plugins
+
+import io.ktor.server.application.Application
+import ktp.fr.data.model.Heroes
+import ktp.fr.data.model.Tracks
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
+fun Application.initDatabase() {
+    val username: String = environment.config.property("db.user").getString()
+    val pwd: String = environment.config.property("db.pwd").getString()
+    val db: String = environment.config.property("db.name").getString()
+    val driverClassName = "com.mysql.cj.jdbc.Driver"
+    val jdbcURL = "jdbc:mysql://localhost:3306/${db}?user=${username}&password=${pwd}&useSSL=false"
+    val database = Database.connect(jdbcURL, driverClassName, username, pwd)
+    transaction(database) {
+        SchemaUtils.create(Tracks, Heroes)
+    }
+}
--- src/main/kotlin/ktp/fr/plugins/Serialization.kt
+++ src/main/kotlin/ktp/fr/plugins/Serialization.kt
@@ -2,13 +2,15 @@ package ktp.fr.plugins
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.auth.jwt.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
+import io.ktor.http.HttpStatusCode
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+import io.ktor.server.auth.Authentication
+import io.ktor.server.auth.jwt.JWTPrincipal
+import io.ktor.server.auth.jwt.jwt
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.server.response.respond
 
 
 fun Application.configureSerialization() {
